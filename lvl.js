afa2046ab4cc510decb0adcef169b2b9816b881b
MaxenceME <maxence@mmapi.fr>
2021-01-19 17:34:26 +0100

    Add files via upload

---
+++ lvl.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+module.exports.lvl = (lvl = null) => {
+    let self = this;
+    this._lvl = lvl;
+
+    return {
+        checkAccess: (req = null, res = null, next = null) => {
+            if (req === null) {
+                throw new Error('Houston we got a pwoblem !');
+            }
+
+            if (req['jwt-check'].key === null || req['jwt-check'].roleColumnName === null) {
+                throw new Error('JWT CHECK : params key and roleColumnName must be set')
+            }
+
+            let token = req.headers.authentication;
+            if (token !== undefined) {
+                jwt.verify(token, req['jwt-check'].key, (err, payload) => {
+                    if (err) {
+                        res.sendStatus(401);
+                    } else {
+                        if (self._lvl !== null && (payload[req['jwt-check'].roleColumnName] > self._lvl) === req['jwt-check'].roleAsc) {
+                            res.sendStatus(403);
+                        } else {
+                            next();
+                        }
+                    }
+                });
+            } else {
+                res.sendStatus(401);
+            }
+        },
+    };
+}
